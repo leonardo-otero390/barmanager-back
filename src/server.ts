f7deec7d46cff17cd582a18c7ac6dd2e2620bed2
Leonardo Otero <leonardo.otero390@gmail.com>
2022-05-19 17:16:58 -0400

    chore: server with port 5000 always

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 import app from './app.js';
 
-app.listen(process.env.PORT, () => {
+app.listen(5000, () => {
   console.log(
-    `Server is listening on port ${process.env.PORT} at ${process.env.NODE_ENV} mode`
+    `Server is listening on port 5000 at ${process.env.NODE_ENV} mode`
   );
 });
