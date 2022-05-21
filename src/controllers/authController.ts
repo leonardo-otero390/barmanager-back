e5fc9cf5509e8964e022e02239cdc084efab5468
Leonardo Otero <leonardo.otero390@gmail.com>
2022-05-20 23:51:37 -0400

    feat: husky working and docker-composes fixed

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,11 +4,10 @@ import * as customerService from '../services/customerService.js';
 export async function signUp(req: Request, res: Response) {
   await customerService.signUp(req.body);
 
-  res.sendStatus(201);
+  return res.sendStatus(201);
 }
 
 export async function logIn(req: Request, res: Response) {
   const token = await customerService.logIn(req.body);
-
-  res.send({ token });
+  return res.send({ token });
 }
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@
 import app from './app.js';
 
 app.listen(5000, () => {
-  console.log(
-    `Server is listening on port 5000 at ${process.env.NODE_ENV} mode`
-  );
+  console.log('Server is listening on port 5000');
+  if (process.env.NODE_ENV === 'test') {
+    console.log('at test mode');
+  }
 });
