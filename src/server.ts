7fbbf4c0c9db731943f9dea41d18a32c50c9af94
Leonardo Otero <leonardo.otero390@gmail.com>
2022-06-03 13:02:27 -0400

    chore: fix port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import app from './app.js';
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
   if (process.env.NODE_ENV === 'test') {
     console.log('at test mode');
