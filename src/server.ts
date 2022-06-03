292e5b1c137845161ac9bc15133145abdf058b98
Leonardo Otero <leonardo.otero390@gmail.com>
2022-06-03 11:25:24 -0400

    chore: PORT comes from .env

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import app from './app.js';
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
+  console.log(`Server is listening on port ${process.env.PORT}`);
   if (process.env.NODE_ENV === 'test') {
     console.log('at test mode');
   }
